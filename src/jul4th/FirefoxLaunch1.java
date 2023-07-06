c3034f4cc04ccc54608358e3d9fe0f7e2ca0176f
seleniumriyaz <selenium.riyaz.shared@gmail.com>
2023-07-06 22:01:27 +0530

    Jul 6th Notes

---
+++ src/jul4th/FirefoxLaunch1.java
@@ -0,0 +1,13 @@
+package jul4th;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FirefoxLaunch1 {
+
+	public static void main(String[] args) {
+		
+		new FirefoxDriver();
+
+	}
+
+}
